b934909bb507857044ed90d59c3d41f1ca98579c
ummetarin <ummehomairatarin@gmail.com>
2023-10-19 16:38:08 +0600

    Added Banner into website

---
+++ src/Home/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <img className='mx-auto w-full' src="/public/image/bannerpic.jpg" alt="" />
+        </div>
+    );
+};
+
+export default Banner;
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Banner></Banner>
             <Outlet></Outlet>
         </div>
     );
